dcb35f7cf912b06c4e641b4ff1ac51c1e6fcc2a9
alexander papageorge <alexander.papageorge@openlabsusa.org>
2022-07-06 22:52:44 -0400

    replicate first simulation

---
+++ curation_sim/main.js
@@ -0,0 +1,8 @@
+import {x, y} from './modules/test.js';
+import {curationPool, state, sim_result} from './modules/utils.js';
+
+console.log("hello");
+console.log(x);
+console.log(y);
+console.log(JSON.stringify(sim_result, null, 2));
+
---
+++ curation_sim/modules/test.js
@@ -0,0 +1,5 @@
+import _ from "lodash";
+
+export const x = 8;
+export const y = _.capitalize("toast")
+
