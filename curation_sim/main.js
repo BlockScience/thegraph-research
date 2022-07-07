f383baecc086e9bd7ac2c19dc8ffafa0da1eb267
alexander papageorge <alexander.papageorge@openlabsusa.org>
2022-07-06 23:02:34 -0400

    a little prettier

--- curation_sim/main.js
+++ curation_sim/main.js
@@ -1,8 +1,3 @@
-import {x, y} from './modules/test.js';
-import {curationPool, state, sim_result} from './modules/utils.js';
+import {sim_result} from './modules/utils.js';
 
-console.log("hello");
-console.log(x);
-console.log(y);
 console.log(JSON.stringify(sim_result, null, 2));
-
--- curation_sim/modules/test.js
+++
@@ -1,5 +0,0 @@
-import _ from "lodash";
-
-export const x = 8;
-export const y = _.capitalize("toast")
-
